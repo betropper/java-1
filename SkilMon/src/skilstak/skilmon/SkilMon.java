8416a521cd3543222a697f2afcc60f2a4c0ab963
Benjamin Muhlestein <betropper@gmail.com>
2015-09-25 18:59:10 -0400

    save

---
+++ SkilMon/src/skilstak/skilmon/SkilMon.java
@@ -0,0 +1,17 @@
+package skilstak.skilmon;
+
+
+import java.util.ArrayList
+
+final public class SkilMon {
+
+    public int attack, catchRate, defense, exp, happiness
+           spAttack, spDefence, speed, total, evolvesAt;
+
+    public String species;
+
+    public double catchRate, growthRate, height, width, mfRatio
+
+    public ArrayList<String> abilities, moves, types;
+
+}
